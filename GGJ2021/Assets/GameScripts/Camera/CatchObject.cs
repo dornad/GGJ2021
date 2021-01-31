a3e293fe76d1e3c637e5b77358b20ad90a90ea59
Andres Castro <andres.castro@joycomgames.com>
2021-01-30 21:15:43 -0500

    Feat_GameScene 1
    Feat_GameScene 1
    Add files like a textures, models and some scripts

---
+++ GGJ2021/Assets/GameScripts/Camera/CatchObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchObject : MonoBehaviour
+{
+	private Transform takeObject;
+	private bool catched = false;
+	Transform cursor;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Cursor") FindObjectOfType<UIController>().ShowExamLabel(true);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Cursor") FindObjectOfType<UIController>().ShowExamLabel(false);
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Cursor")
+		{
+			if (Input.GetMouseButton(0) && !catched)
+			{
+				takeObject = GameObject.FindGameObjectWithTag("TakeObject").transform;
+				GetComponent<Rigidbody>().isKinematic = true;
+				cursor = other.transform;
+				cursor.gameObject.SetActive(false);
+				FindObjectOfType<UIController>().ShowExamLabel(false);
+				catched = true;
+			}
+		}
+	}
+
+	private void LateUpdate()
+	{
+		if (catched && takeObject)
+		{
+			transform.position = takeObject.position;
+
+			if (Input.GetMouseButtonUp(0))
+			{
+				takeObject = null;
+				GetComponent<Rigidbody>().isKinematic = false;
+
+				StartCoroutine(ReactiveCursor());
+			}
+		}
+	}
+
+	IEnumerator ReactiveCursor()
+	{
+		yield return new WaitForSeconds(0.5f);
+		cursor.gameObject.SetActive(true);
+		catched = false;
+	}
+}
---
+++ GGJ2021/Assets/GameScripts/Camera/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+	public Transform examLabel;
+	public RawImage textureToShow;
+
+	public void ShowExamLabel(bool show)
+	{
+		examLabel.gameObject.SetActive(show);
+	}
+
+	public void ShowTexture(Texture2D toShow)
+	{
+		textureToShow.texture = toShow;
+		textureToShow.SetNativeSize();
+		textureToShow.gameObject.SetActive(true);
+	}
+
+	public void HideTexture()
+	{
+		textureToShow.gameObject.SetActive(false);
+	}
+}
